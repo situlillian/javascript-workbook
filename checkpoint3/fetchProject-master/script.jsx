d940a9e3391b3899ee53ce1a99d3e78a5f488838
situlillian <mslilliansitu@gmail.com>
2017-09-13 17:45:56 -0500

    Added NASA APOD data

--- checkpoint3/fetchProject-master/script.jsx
+++ checkpoint3/fetchProject-master/script.jsx
@@ -8,17 +8,42 @@ class Fetch extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      apod: []
+    };
   }
 
   componentWillMount() {
   // Perhaps fetch some data here
+    fetch('https://api.nasa.gov/planetary/apod?api_key=odtesQqkEds5ctqCTy75uK0QV0QVII2uOB0ffeII').then((response) => {
+      response.json().then((data) => {
+        this.setState({
+          apod: data
+        });
+
+        console.log(this.state.apod);
+      });
+    });
   }
 
   render() {
     return (
-      <h1>Sample Blog</h1>
-      // Your code here
+      <div className='container'>
+        <div className='row'>
+          <div className='col'>
+            <div className='card text-white'>
+              <img className='img-fluid' src={this.state.apod.hdurl} alt={this.state.apod.title} />
+              <div className='card-img-overlay'>
+                <h1 className='card-title'>{this.state.apod.title}</h1>
+                <h4 className='card-text'>by {this.state.apod.copyright}</h4>
+                <p className='card-text'>{this.state.apod.explanation}</p>
+                <p className='card-text'>Date: {this.state.apod.date}</p>
+                <button type='button' className='btn btn-sm btn-outline-secondary'>LEARN MORE</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     );
   }
 }
