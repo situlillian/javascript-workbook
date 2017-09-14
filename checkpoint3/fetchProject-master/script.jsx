31146d9cd05507e40b065ef51b57291a119ee4a3
situlillian <mslilliansitu@gmail.com>
2017-09-13 21:45:15 -0500

    Added styling, todo: button

--- checkpoint3/fetchProject-master/script.jsx
+++ checkpoint3/fetchProject-master/script.jsx
@@ -9,37 +9,39 @@ class Fetch extends React.Component {
     super(props);
 
     this.state = {
-      apod: []
+      apod: [],
+      search: []
     };
   }
 
   componentWillMount() {
-  // Perhaps fetch some data here
     fetch('https://api.nasa.gov/planetary/apod?api_key=odtesQqkEds5ctqCTy75uK0QV0QVII2uOB0ffeII').then((response) => {
       response.json().then((data) => {
         this.setState({
           apod: data
         });
-
-        console.log(this.state.apod);
       });
     });
   }
 
   render() {
     return (
-      <div className='container'>
+      <div className='main' style={{backgroundImage: 'url(' + this.state.apod.hdurl + ')'}}>
+        <nav className='navbar navbar-dark text-center'>
+          <span className='navbar-brand mb-0'>
+            <p className='text-uppercase'>NASA Astronomy Photo of the Day</p>
+          </span>
+        </nav>
         <div className='row'>
           <div className='col'>
-            <div className='card text-white'>
-              <img className='img-fluid' src={this.state.apod.hdurl} alt={this.state.apod.title} />
-              <div className='card-img-overlay'>
-                <h1 className='card-title'>{this.state.apod.title}</h1>
-                <h4 className='card-text'>by {this.state.apod.copyright}</h4>
-                <p className='card-text'>{this.state.apod.explanation}</p>
-                <p className='card-text'>Date: {this.state.apod.date}</p>
-                <button type='button' className='btn btn-sm btn-outline-secondary'>LEARN MORE</button>
-              </div>
+            <div className='card'>
+              <h1 className='card-title'>{this.state.apod.title}</h1>
+              <h4 className='card-text text-uppercase'>by {this.state.apod.copyright}</h4>
+              <p className='card-text'>{this.state.apod.explanation}</p>
+              <p className='card-text'>Date: {this.state.apod.date}</p>
+              {/* <a className='btn btn-outline-secondary' href='#' role="button">
+                <p>Learn More</p>
+              </a> */}
             </div>
           </div>
         </div>
